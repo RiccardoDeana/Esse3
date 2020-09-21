7a461e758294161318630ca2da5c3607dbd0c17e
RiccardoDeana <riccardodeana93@gmail.com>
2020-09-21 18:59:04 +0200

    Sembra che funzioni tutto. Manca il logout e la scadenza del token

--- controllers/regGradeController.js
+++ controllers/regGradeController.js
@@ -7,24 +7,29 @@ function regGradeGET (req, res, next) {
 
 function regGradePOST (req, res, next) {
     if (!req.body) return res.sendStatus(400);
-    const dati = req.body;
+    const dati = {
+        "studente": req.body.studente,
+        "esame": req.body.esame,
+        "voto": req.body.voto,
+        "lode": req.body.lode === 'on' ? true : false
+    };
     Registration.find({studente:dati.studente, nomeEsame:dati.esame})
         .then(registrations => {
-            if(registrations){
-                Registration.deleteMany({studente:dati.studente, nomeEsame:dati.esame}, function (err) {
+            if(registrations[0]){
+                const passed = new Passed(dati);
+                passed.save(function(err){
                     if(err){
-                        return next(err)
+                        const error = new Error('Dati incongruenti o voto già registrato');
+                        error.status = 401;
+                        return next(error);
+                    }else{
+                        Registration.deleteMany({studente:dati.studente, nomeEsame:dati.esame}, function (err) {
+                            if(err){
+                                return next(err)
+                            }
+                        });
+                        res.redirect('/regGrade');
                     }
-                    const passed = new Passed(dati);
-                    passed.save(function(err){
-                        if(err){
-                            const error = new Error('Dati incongruenti');
-                            error.status = 401;
-                            return next(error);
-                        }else{
-                            res.redirect('/regGrade');
-                        }
-                    });
                 });
             }else{
                 const error = new Error('Lo studente non si è iscritto a questo esame');

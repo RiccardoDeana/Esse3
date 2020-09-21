6cb914234d7b716a11c252a4e6863e96a8258de3
RiccardoDeana <riccardodeana93@gmail.com>
2020-09-21 03:16:11 +0200

    ergesvsv

---
+++ controllers/remStudentController.js
@@ -0,0 +1,32 @@
+const Student = require('../models/student');
+
+function remStudentGET (req, res, next) {
+    res.render('./remStudent');
+}
+
+function remStudentPOST (req, res, next) {
+    if (!req.body) return res.sendStatus(400);
+    Student.findOne({matricola:req.body.matricola})
+        .then(student => {
+            if(student){
+                Student.deleteOne({matricola:req.body.matricola})
+                    .then(() =>{
+                        res.redirect('/remStudent');
+                    })
+                    .catch(error => {
+                        return next(error)});
+            }
+            else{
+                const error = new Error('Lo studente non esiste');
+                error.status = 401;
+                return next(error);
+            }
+        })
+        .catch(error => {
+            return next(error)});
+}
+
+module.exports = {
+    remStudentGET,
+    remStudentPOST
+};
--- routers/remStudentRouter.js
+++ routers/remStudentRouter.js
@@ -2,16 +2,16 @@ const express = require('express');
 const router = express.Router();
 const configClearError = require('../middleware/configClearError');
 const configError = require('../middleware/configError');
-const bodyParser= require('body-parser');
-router.use(bodyParser.urlencoded({ extended: true }));
+const isLoggedAdmin = require('../middleware/isLoggedAdmin');
 
-function indexRouter (controller) {
 
+function remStudentRouter (controller) {
     router.route('/remStudent')
+        .all(isLoggedAdmin)
         .get(configClearError('remStudent'), controller.remStudentGET)
         .post(controller.remStudentPOST, configError('remStudent'));
 
     return router;
 }
 
-module.exports = indexRouter;
+module.exports = remStudentRouter;

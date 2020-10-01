c506ca60592838d5cb6594ebd6a991e299798adf
Riccardo Deana <riccardodeana93@gmail.com>
2020-10-01 23:58:03 +0200

    Modificato indexRouter.js: ora tornando alla home (localhost:3000/) non serve rifare il login

--- routers/indexRouter.js
+++ routers/indexRouter.js
@@ -3,13 +3,28 @@
 const express = require('express');
 const router = express.Router();
 const configClearError = require('../middleware/configClearError');
+const AdminToken = require('../models/adminToken');
+const StudentToken = require('../models/studentToken');
 
 function indexRouter (controller) {
-    router.get('/', function(req, res) {
-        res.redirect('/login');
+
+    router.get('/', async function(req, res) {
+        const token = req.app.locals.token;
+        const student = await StudentToken.findOne({token:token});
+        const admin = await AdminToken.findOne({token:token});
+        if(student){
+            res.redirect('/exams');
+        }else if(admin){
+            res.redirect('/addStudent');
+        }else{
+            res.redirect('/login');
+        }
     });
+
     router.get('/login', configClearError('login'), controller.loginPage);
+
     router.post('/login', controller.firstPage);
+
     router.post('/logout', controller.logout);
 
     return router;

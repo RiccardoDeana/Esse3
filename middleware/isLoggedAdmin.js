6c84a1ec3a45fec9f34f284cad62ecb3963cb79a
Riccardo Deana <riccardodeana93@gmail.com>
2020-10-31 18:12:44 +0100

    Risolto un bug che faceva crashare tutto in un caso si tentasse di accedere ad una specifica pagina senza autorizzazione

--- middleware/isLoggedAdmin.js
+++ middleware/isLoggedAdmin.js
@@ -10,28 +10,34 @@ const errorRedirect = require('../messages/errorRedirect');
 // altrimenti rimanda alla pagina di login
 async function isLoggedAdmin (req, res, next) {
     const token = req.signedCookies.token;
-    const data = jwt.verify(token, process.env.JWT_KEY);
-    const adminToken = await AdminToken.findOne({ idAdmin: data._id, token: token});
-    if(adminToken) {
-        const admin = await Admin.findOne({_id: adminToken.idAdmin});
-        if(admin){
-            const result = await admin.logOut(token);
-            if (result) {
-                admin.generateAuthToken()
-                    .then((tok) => {
-                        res.cookie('token', tok, { signed: true, overwrite: true, maxAge: 900000 });
-                        return next();
-                    })
-                    .catch(error => {
-                        errorRedirect('login',error, req, res);
-                    })
+    if(token){
+        const data = jwt.verify(token, process.env.JWT_KEY);
+        const adminToken = await AdminToken.findOne({ idAdmin: data._id, token: token});
+        if(adminToken) {
+            const admin = await Admin.findOne({_id: adminToken.idAdmin});
+            if(admin){
+                const result = await admin.logOut(token);
+                if (result) {
+                    admin.generateAuthToken()
+                        .then((tok) => {
+                            res.cookie('token', tok, { signed: true, overwrite: true, maxAge: 900000 });
+                            return next();
+                        })
+                        .catch(error => {
+                            errorRedirect('login',error, req, res);
+                        })
+                }
+            }else {
+                errorRedirect('login', 'Utente non esiste', req, res);
             }
         }else {
-            errorRedirect('login', 'Utente non esiste', req, res);
+            errorRedirect('login', 'Sessione scaduta. Per accedere devi eseguire il login', req, res);
         }
-    }else {
-        errorRedirect('login', 'Sessione scaduta. Per accedere devi eseguire il login', req, res);
+    }else{
+        errorRedirect('login', 'Non sei autorizzato ad accedere a questa risorsa', req, res);
     }
+
+
 }
 
 module.exports = isLoggedAdmin;
--- middleware/isLoggedStudent.js
+++ middleware/isLoggedStudent.js
@@ -10,28 +10,33 @@ const errorRedirect = require('../messages/errorRedirect');
 // altrimenti rimanda alla pagina di login
 async function isLoggedStudent (req, res, next) {
     const token = req.signedCookies.token;
-    const data = jwt.verify(token, process.env.JWT_KEY);
-    const studentToken = await StudentToken.findOne({ idStudente: data._id, token: token});
-    if(studentToken) {
-        const student = await Student.findOne({_id: studentToken.idStudente});
-        if(student){
-            const result = await student.logOut(token);
-            if (result) {
-                student.generateAuthToken()
-                    .then((tok) => {
-                        res.cookie('token', tok, { signed: true, overwrite: true, maxAge: 900000 });
-                        return next();
-                    })
-                    .catch(error => {
-                        errorRedirect('login',error, req, res);
-                    })
+    if(token){
+        const data = jwt.verify(token, process.env.JWT_KEY);
+        const studentToken = await StudentToken.findOne({ idStudente: data._id, token: token});
+        if(studentToken) {
+            const student = await Student.findOne({_id: studentToken.idStudente});
+            if(student){
+                const result = await student.logOut(token);
+                if (result) {
+                    student.generateAuthToken()
+                        .then((tok) => {
+                            res.cookie('token', tok, { signed: true, overwrite: true, maxAge: 900000 });
+                            return next();
+                        })
+                        .catch(error => {
+                            errorRedirect('login',error, req, res);
+                        })
+                }
+            }else {
+                errorRedirect('login', 'Utente non esiste', req, res);
             }
         }else {
-            errorRedirect('login', 'Utente non esiste', req, res);
+            errorRedirect('login', 'Sessione scaduta. Per accedere devi eseguire il login', req, res);
         }
-    }else {
-        errorRedirect('login', 'Sessione scaduta. Per accedere devi eseguire il login', req, res);
+    }else{
+        errorRedirect('login', 'Non sei autorizzato ad accedere a questa risorsa', req, res);
     }
+
 }
 
 
